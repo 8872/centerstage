07efe09298174447dce1d703fbf24298d4662c6b
DevMello <69360966+DevMello@users.noreply.github.com>
2023-09-28 19:13:10 -0700

    basic commands for all the subsystems

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command/arm/SetArm.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.command.arm;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import org.firstinspires.ftc.teamcode.subsystem.ArmSys;
+
+public class SetArm extends CommandBase {
+    private final ArmSys sys;
+    private final ArmSys.ArmPosition position;
+    public SetArm(ArmSys sys, ArmSys.ArmPosition position) {
+        this.sys = sys;
+        this.position = position;
+        addRequirements(sys);
+    }
+    @Override
+    public void initialize() {
+        sys.arm(position);
+    }
+    @Override
+    public boolean isFinished() {
+        return sys.getPos() == position;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/command/claw/SetClaw.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.command.claw;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import org.firstinspires.ftc.teamcode.subsystem.ClawSys;
+
+public class SetClaw extends CommandBase {
+    private final ClawSys sys;
+    private final ClawSys.ClawPosition position;
+    public SetClaw(ClawSys sys, ClawSys.ClawPosition position) {
+        this.sys = sys;
+        this.position = position;
+        addRequirements(sys);
+    }
+    @Override
+    public void initialize() {
+        sys.claw(position);
+    }
+    @Override
+    public boolean isFinished() {
+        return sys.getClaw() == position;
+    }
+}
